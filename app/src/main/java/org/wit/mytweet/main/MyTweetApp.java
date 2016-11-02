fc30244dc3fb3d1fc970fb49714592bb1139478b
John Kennedy <johnwjken@gmail.com>
2016-11-02 18:20:13 +0000

    Refactored DbHelper, MyTweetApp class & removed the User model

--- app/src/main/java/org/wit/mytweet/main/MyTweetApp.java
+++ app/src/main/java/org/wit/mytweet/main/MyTweetApp.java
@@ -7,8 +7,6 @@ import org.wit.mytweet.models.Portfolio;
 import org.wit.mytweet.models.PortfolioSerializer;
 import org.wit.mytweet.sqlite.DbHelper;
 
-import static org.wit.android.helpers.LogHelpers.info;
-
 /**
  * Created by User on 02/10/2016.
  */
@@ -25,12 +23,10 @@ public class MyTweetApp extends Application {
     public void onCreate() {
         super.onCreate();
         app = this;
-        dbHelper = new DbHelper(getApplicationContext());
         Log.d(TAG, "MyTweet app launched");
         PortfolioSerializer serializer = new PortfolioSerializer(this, FILENAME);
         portfolio = new Portfolio(serializer);
 
-        info(this, "TweetListActivity app launched");
 
 
     }
--- app/src/main/java/org/wit/mytweet/sqlite/DbHelper.java
+++ app/src/main/java/org/wit/mytweet/sqlite/DbHelper.java
@@ -8,150 +8,74 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import org.wit.mytweet.models.User;
-
-import java.util.ArrayList;
-
-import java.util.List;
-import java.util.UUID;
-
 /**
  * Created by User on 01/11/2016.
  */
 
 public class DbHelper extends SQLiteOpenHelper {
 
-    static final String TAG = "DbHelper";
-    static final String DATABASE_NAME = "users.db";
+    static final String TAG = DbHelper.class.getSimpleName();
     static final int DATABASE_VERSION = 1;
-    static final String TABLE_USERS = "tableUsers";
+    static final String DATABASE_NAME = "mytweet.db";
 
+    static final String USER_TABLE = "users";
     static final String PRIMARY_KEY = "id";
-    static final String FIRSTNAME = "firstName";
-    static final String LASTNAME = "lastName";
-    static final String EMAIL = "email";
-    static final String PASSWORD = "password";
+    static final String COLUMN_FIRSTNAME = "firstName";
+    static final String COLUMN_LASTNAME = "lastName";
+    static final String COLUMN_EMAIL = "email";
+    static final String COLUMN_PASSWORD = "password";
 
-
-    Context context;
+    public static final String CREATE_TABLE_USERS = "CREATE TABLE " + USER_TABLE + "("
+            + PRIMARY_KEY + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COLUMN_FIRSTNAME + " TEXT,"
+            + COLUMN_LASTNAME + " TEXT,"
+            + COLUMN_EMAIL + " TEXT,"
+            + COLUMN_PASSWORD + " TEXT);";
 
     public DbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        this.context = context;
+
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTable =
-                "CREATE TABLE tableUsers " +
-                        "(id text primary key, " +
-                        "firstName text)" +
-                        "lastName text," +
-                        "email text," +
-                        "password text,";
-
-        db.execSQL(createTable);
-        Log.d(TAG, "DbHelper.onCreated: " + createTable);
+        db.execSQL(CREATE_TABLE_USERS);
     }
 
     /**
-     * @param user Reference to User object to be added to database
+     *  Reference to User object to be added to database
      */
-    public void addUser(User user) {
+    public void addUser(String firstName, String lastName, String email, String password) {
         SQLiteDatabase db = this.getWritableDatabase();
+
         ContentValues values = new ContentValues();
-        values.put(PRIMARY_KEY, user.id.toString());
-        values.put(FIRSTNAME, user.firstName);
+        values.put(COLUMN_FIRSTNAME, firstName);
+        values.put(COLUMN_LASTNAME, lastName);
+        values.put(COLUMN_EMAIL, email);
+        values.put(COLUMN_PASSWORD, password);
 
-        values.put(LASTNAME, user.lastName);
-        values.put(EMAIL, user.email);
-        values.put(PASSWORD, user.password);
         // Insert record
-        db.insert(TABLE_USERS, null, values);
+        long id = db.insert(USER_TABLE, null, values);
         db.close();
+
+        Log.d(TAG, "user inserted" + id);
     }
 
-    public User selectUser(UUID userId) {
-        User user;
+
+    public boolean getUser(String email, String password) {
+        String selectQuery = "SELECT * from " + USER_TABLE + " WHERE " +
+                COLUMN_EMAIL + " = " + "'" + email + "'" + " AND " + COLUMN_PASSWORD + " = " + "'" + password + "'";
+
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = null;
+        Cursor cursor = db.rawQuery(selectQuery, null);
 
-        try {
-            user = new User();
-
-            cursor = db.rawQuery("SELECT * FROM tableUsers WHERE id = ?", new String[]{userId.toString() + ""});
-
-            if (cursor.getCount() > 0) {
-                int columnIndex = 0;
-                cursor.moveToFirst();
-                user.id = UUID.fromString(cursor.getString(columnIndex++));
-                user.firstName = cursor.getString(columnIndex++);
-                user.lastName = cursor.getString(columnIndex++);
-                user.email = cursor.getString(columnIndex++);
-                user.password = cursor.getString(columnIndex++);
-
-            }
-        } finally {
-            cursor.close();
-        }
-        return user;
-    }
-
-    public void deleteUser(User user) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        try {
-            db.delete("tableUsers", "id" + "=?", new String[]{user.id.toString() + ""});
-        } catch (Exception e) {
-            Log.d(TAG, "delete user failure: " + e.getMessage());
-        }
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if exists " + TABLE_USERS);
-        Log.d(TAG, "onUpdated");
-        onCreate(db);
-    }
-
-    /**
-     * Query database and select entire tableUsers.
-     *
-     * @return A list of User object records
-     */
-    public List<User> selectAllUsers() {
-        List<User> users = new ArrayList<User>();
-        String query = "SELECT * FROM " + "tableUsers";
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery(query, null);
-        if (cursor.moveToFirst()) {
-            int columnIndex = 0;
-            do {
-                User user = new User();
-                user.id = UUID.fromString(cursor.getString(columnIndex++));
-                user.firstName = cursor.getString(columnIndex++);
-                user.lastName = cursor.getString(columnIndex++);
-                user.email = cursor.getString(columnIndex++);
-                user.password = cursor.getString(columnIndex++);
-
-                columnIndex = 0;
-
-                users.add(user);
-            } while (cursor.moveToNext());
+        cursor.moveToFirst();
+        if (cursor.getCount() > 0) {
+            return true;
         }
         cursor.close();
-        return users;
-    }
-
-    /**
-     * Delete all records
-     */
-    public void deleteAllUsers() {
-        SQLiteDatabase db = this.getWritableDatabase();
-        try {
-            db.execSQL("delete from tableUsers");
-        } catch (Exception e) {
-            Log.d(TAG, "delete users failure: " + e.getMessage());
-        }
+        db.close();
+        return false;
     }
 
     /**
@@ -161,28 +85,16 @@ public class DbHelper extends SQLiteOpenHelper {
      */
     public long getCount() {
         SQLiteDatabase db = this.getReadableDatabase();
-        long numberRecords = DatabaseUtils.queryNumEntries(db, TABLE_USERS);
+        long numberRecords = DatabaseUtils.queryNumEntries(db, USER_TABLE);
         db.close();
         return numberRecords;
     }
 
-    /**
-     * Update an existing User record.
-     * All fields except record id updated.
-     *
-     * @param user The User record being updated.
-     */
-    public void updateUser(User user) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        try {
-            ContentValues values = new ContentValues();
-            values.put(FIRSTNAME, user.firstName);
-            values.put(LASTNAME, user.lastName);
-            values.put(EMAIL, user.email);
-            values.put(PASSWORD, user.password);
-            db.update("tableUsers", values, "id" + "=?", new String[]{user.id.toString() + ""});
-        } catch (Exception e) {
-            Log.d(TAG, "update users failure: " + e.getMessage());
-        }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + USER_TABLE);
+        onCreate(db);
     }
+
+
 }
