c9e63ce2afd147e844b0d5cd3e1417a77250b3ac
John Kennedy <johnwjken@gmail.com>
2016-11-02 18:23:52 +0000

    Validated and save new users into database

--- app/src/main/java/org/wit/mytweet/activities/SignupActivity.java
+++ app/src/main/java/org/wit/mytweet/activities/SignupActivity.java
@@ -8,9 +8,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.wit.android.helpers.IntentHelper;
 import org.wit.mytweet.R;
 import org.wit.mytweet.main.MyTweetApp;
-import org.wit.mytweet.models.User;
+import org.wit.mytweet.sqlite.DbHelper;
 
 import static org.wit.android.helpers.LogHelpers.info;
 import static org.wit.mytweet.R.id.Email;
@@ -22,20 +23,25 @@ import static org.wit.mytweet.R.id.lastName;
  * Created by User on 02/10/2016.
  */
 
-public class SignupActivity extends AppCompatActivity {
-
-    MyTweetApp app;
-    User user;
-
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
     private Button bsignup;
     private EditText et_firstname, et_lastname, et_email, et_password, et_cpassword;
     private String firstname, lastname, email, password, cpassword;
+
+    MyTweetApp app;
+    private DbHelper db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         info(this, "SignupActivity page opened");
+
+
+        app = MyTweetApp.getApp();
+        db = new DbHelper(this);
+
 
         et_firstname = (EditText) findViewById(firstName);
         et_lastname = (EditText) findViewById(lastName);
@@ -44,15 +50,13 @@ public class SignupActivity extends AppCompatActivity {
         et_cpassword = (EditText) findViewById(R.id.ConfirmPassword);
 
         bsignup = (Button) findViewById(R.id.signup);
-        bsignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                register();
-            }
-        });
-
+        bsignup.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        register();
+    }
 
     public void register() {
         initialise();
@@ -64,9 +68,9 @@ public class SignupActivity extends AppCompatActivity {
     }
 
     public void onSignupSuccess() {
-        user = new User();
-
-        app.dbHelper.addUser(user);
+        db.addUser(firstname, lastname, email, password);
+        Toast.makeText(this, "User registered", Toast.LENGTH_SHORT).show();
+        IntentHelper.startActivity(this, LoginActivity.class);
     }
 
     public boolean validate() {
@@ -84,15 +88,16 @@ public class SignupActivity extends AppCompatActivity {
             valid = false;
         }
         if (password.isEmpty()) {
-            et_firstname.setError("Please enter a Password");
+            et_password.setError("Please enter a Password");
             valid = false;
         }
         if (cpassword.isEmpty()) {
-            et_lastname.setError("Please enter a Password ");
+            et_cpassword.setError("Please enter a Password ");
             valid = false;
         }
         if (!password.equals(cpassword)) {
             Toast.makeText(this, "your password did not match", Toast.LENGTH_SHORT).show();
+            valid = false;
         }
 
         return valid;
@@ -105,4 +110,6 @@ public class SignupActivity extends AppCompatActivity {
         password = et_password.getText().toString().trim();
         cpassword = et_cpassword.getText().toString().trim(); // trim used to remove any whitespace at the end if present
     }
+
+
 }
